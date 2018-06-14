b4dc550534d1043a2f114d6390a9c9bddc875c8b
Amanda Christianson <amanda.christianson@telia.com>
2018-06-14 22:36:29 +0200

    With working opening, saving and clearing buttons in addition to a non-functioning dropdown menu

--- GUITutorial.py
+++ GUITutorial.py
@@ -1,133 +1,95 @@
-#Time and date
+#Amanda Christianson
+#2018-06
+
+#Fix so that the program ends correctly when pressing the "Cancel" button in open and save file
+
 
 #!/usr/in/python3 #Defines where the interpretor is located. Tells the operating system that it is a python script?
 # -*- coding: utf-8 -*- #Necessary?
 
 import sys #Necessary?
-import os
-#from PyQt5.QtCore import QDate, QTime, QDateTime, Qt #Why should "Qt" be imported? What does it stand for?
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QLabel, QComboBox, QTextEdit, QFileDialog, QVBoxLayout, QHBoxLayout
+import os #Enables the use of the file dialoge
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QComboBox, QTextEdit, QFileDialog, QVBoxLayout, QHBoxLayout
 from PyQt5.QtGui import QFont
-
-"""
-def date_time():
-    now = QDate.currentDate() #Defines a variable (or function?) "now" with the current date.
-
-    #Uses the variable "now" to print the current date in different ways
-    print("Date as ISO: ", now.toString(Qt.ISODate)) #The toString simply returns the date as a string. ISO stands for "international date format"
-    print("Date as set on this computer: ", now.toString(Qt.DefaultLocaleLongDate)) #Local refers to the local time zone in which the computer is set (?)
-
-    datetime = QDateTime.currentDateTime() #Declares a variable (?) "datetime" with both the date and time
-
-    print("The date and time: ", datetime.toString())
-
-    time = QTime.currentTime()
-
-    print("The time as set on this computer", time.toString(Qt.DefaultLocaleLongDate)) #Why should this way of converting to string be used?
-"""
 
 def small_window():
     class Window(QWidget):
 
-        def __init__(self):
-            super(Window, self).__init__()
+        def __init__(self): #Creates an init method (private?)
+            super(Window, self).__init__() #Super return the parent object of the class
 
             self.text = QTextEdit(self) #To enable the text editor
-            self.clearButton = QPushButton('Clear') #Clears a document
-            self.runButton = QPushButton('Run')
+
+            #Creates buttons
+            self.openButton = QPushButton('Open') #Opens a text file
+            self.clearButton = QPushButton('Clear') #Clears a text file
+            self.saveButton = QPushButton('Save') #Saves the text file
+            self.runButton = QPushButton('Run') #Runs the hypothetical test
+
+            self.dropDown = QComboBox() #Creates a dropdown menu
+
+            #Declares what will be shown when the button is hovered over
             self.runButton.setToolTip('Press to run the test')
-            self.saveButton = QPushButton('Save')
             self.saveButton.setToolTip('Press to save the changes')
-            self.openButton = QPushButton('Open')
             self.openButton.setToolTip('Press to open text file')
 
             self.initUI()
 
         def initUI(self):
-            vLayout = QVBoxLayout()
-            hLayout = QHBoxLayout()
+            vLayout = QVBoxLayout() #Creates a vertical box layout
+            hLayout = QHBoxLayout() #Creates a horisontal box layout
 
-            vLayout.addWidget(self.openButton)
-            self.openButton.clicked.connect(self.open_text)
-            vLayout.addWidget(self.text)
+            #Places a dropdown menu (not get fully functioning)
+            vLayout.addWidget(self.dropDown)
+            self.dropDown.addItem('File 1')
+            self.dropDown.addItem('File 2')
+            self.dropDown.addItem('File 3')
+
+            vLayout.addWidget(self.openButton) #Places the open button
+
+            vLayout.addWidget(self.text) #Places the text editor
+
+            #Places buttons in the window
             hLayout.addWidget(self.clearButton)
-            self.clearButton.clicked.connect(self.clear_text)
-            hLayout.addWidget(self.runButton)
             hLayout.addWidget(self.saveButton)
+            hLayout.addWidget(self.runButton)
+
+            #Assignes what will happen when the buttons are clicked on
+            self.openButton.clicked.connect(self.open_text)
+            self.clearButton.clicked.connect(self.clear_text)
             self.saveButton.clicked.connect(self.save_text)
 
-            vLayout.addLayout(hLayout)
+            vLayout.addLayout(hLayout) #Adds the horisontal layout within the vertical layout
 
-            QToolTip.setFont(QFont('SansSerif', 10)) #Sets the font for the Tooltip elements (the text explaining the function of a widget by popping up when hovering
-
-
-            #Run button
-            #buttonRun = QPushButton('Run', self) #Creates a pushbutton
-            #buttonRun.setToolTip('Press to run the test') #Sets the Tooltip for the button
-            #buttonRun.resize(buttonRun.sizeHint()) #Results in a recommended size for the button
-            #buttonRun.move(100,50) #Moves the button
-
-            #Save button
-            #buttonSave = QPushButton('Save', self) #Creates a pushbutton
-            #buttonSave.setToolTip('Press to save your changes') #Sets the Tooltip för the button
-            #buttonSave.resize(buttonSave.sizeHint()) #Results in a recommended size for the button
-            #buttonSave.move(25,50) #Moves the button
-
-            #Dropdown menu
-            self.lbl = QLabel('Files', self)
-            dropDown = QComboBox(self)
-            dropDown.addItem("File 1")
-            dropDown.addItem("File 2")
-            dropDown.addItem("File 3")
-            dropDown.move(25, 25)
-            self.lbl.move(50,150)
-            dropDown.activated[str].connect(self.on_activated)
+            QToolTip.setFont(QFont('SansSerif', 10)) #Sets the font for the Tooltip elements (the text explaining the function of a widget by popping up when hovered over)
 
             self.setLayout(vLayout)
-            #self.setGeometry(300,300,300,220)
-            self.setWindowTitle('GUI')
-            self.show()
+            self.setWindowTitle('GUI') #Sets the window title
+            self.show() #Shows the window
 
-        def on_activated(self, text):
-            self.lbl.setText(text)
-            self.lbl.adjustSize()
-
-            #Editor
-        def clear_text(self):
+        def clear_text(self): #Clears a text document
             self.text.clear()
 
-        def save_text(self):
-            filename = QFileDialog.getSaveFileName(self, 'Save file', os.getenv('HOME'))
-            with open(filename[0], 'w') as f:
+        def save_text(self): #Lets the user save a document in a chosen folder
+            filename = QFileDialog.getSaveFileName(self, 'Save file', os.getenv('HOME')) #The os.getenv accesses the file dialog (finder)
+            with open(filename[0], 'w') as f: #What does this part mean?
                 my_text = self.text.toPlainText()
                 f.write(my_text)
 
-        def open_text(self):
-            filename = QFileDialog.getOpenFileName(self, 'Open file', os.getenv('HOME'))
-            with open(filename[0], 'r') as f:
+        def open_text(self): #Lets the user choose a file by opening the file dialoge (finder)
+            filename = QFileDialog.getOpenFileName(self, 'Open file', os.getenv('HOME')) #The os.getenv accesses the file dialog (finder)
+            with open(filename[0], 'r') as f: #What does this part mean?
                 file_text = f.read()
                 self.text.setText(file_text)
 
 
     app = QApplication(sys.argv) #Creates an application object. sys.argv is a list of command line arguments.
     ex = Window()
-    sys.exit(app.exec_())
+    sys.exit(app.exec_()) #Ensures that the program ends correctly (?)
 
-"""
-    window = QWidget() #Creates a window (that is, a widget without a parent)
-    window.resize(250, 150) #Resizes the window
-    window.move(300, 300) # Moves the window on the screen (x,y)
-    window.setWindowTitle('Small Window') #Sets the title of the window
-    window.show() #Shows the window on the screen
+def main(): #The main function
 
-    sys.exit(app.exec_()) #Closes the application (?)
-
-"""
-
-def main():
-    #date_time()
-
-    small_window()
+    small_window() #Runs the function that opens the window
 
 if __name__ == '__main__' :
     main()
